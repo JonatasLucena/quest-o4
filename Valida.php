0bbf8214dbd5d39a1e2039412a8aad1d29c362e8
JonatasLucena <93006869+JonatasLucena@users.noreply.github.com>
2021-12-14 15:23:59 -0300

    questão 4

---
+++ Valida.php
@@ -0,0 +1,9 @@
+<?php
+$Nome = $_POST['nome'];
+$Email = $_POST['e-mail'];
+$idade = $_POST['idade'];
+
+echo "NOME: ".$Nome."<br>";
+echo "EMAIL: ".$Email."<br>";
+echo "IDADE: ".$idade."<br>";
+?>
---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title> Questão 4</title>
+</head>
+<body>
+<form method="POST" action="Valida.php">
+	
+	<p> Nome: <input type="text" name="nome"> </p>
+	<p> E-mail: <input type="email" name="e-mail"> </p>
+	<p> Idade <input type="number" name="idade"> </p>
+	<button type="submit"> Inserir Dados</button>
+</form>
+</body>
+</html>
